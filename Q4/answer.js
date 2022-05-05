50f0c3a6491015f953ae2cdc1a7b6c72afb171fb
huisu <huisu0429@naver.com>
2022-05-05 11:06:40 +0900

    4. 콜라츠 추측

--- Q4/answer.js
+++ Q4/answer.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /*
 1937년 Collatz란 사람에 의해 제기된 이 추측은, 주어진 수가 1이 될 때까지 다음 작업을 반복하면,
 모든 수를 1로 만들 수 있다는 추측입니다. 작업은 다음과 같습니다.
@@ -11,17 +11,16 @@
   제한 사항
 입력된 수, num은 1 이상 8000000 미만인 정수입니다.*/
 function solution(num) {
-    let answer = 0;
-    for (let i = 0; i < 500; i++) {
-        // 500번이상시 반복문 종료
-        if (num != 1) {
-            // 1이 아닌 경우
-            num = num % 2 === 0 ? num / 2 : num * 3 + 1;
-        }
-        else {
-            // 1인 경우
-            return (answer = i);
-        }
+  let answer = 0;
+  for (let i = 0; i < 500; i++) {
+    // 500번이상시 반복문 종료
+    if (num != 1) {
+      // 1이 아닌 경우
+      num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    } else {
+      // 1인 경우
+      return (answer = i);
     }
-    return (answer = -1); // 500번 넘었지만, i 값을 얻지 못 함
+  }
+  return (answer = -1); // 500번 넘었지만, i 값을 얻지 못 함
 }
